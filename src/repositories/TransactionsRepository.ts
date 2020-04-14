a41ef82897bcff4b4e1df629879f251b51750209
Jr Bytes <itamazor@gmail.com>
2020-04-14 17:57:09 -0300

    Desafio completado, faz transação vendo se tem saldo, e lista transação com balanço

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,63 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
+// import Balance from '../models/Balance'
+
+enum EType {
+  i = 'income',
+  o = 'outcome',
+}
+
+interface TransactionDTO {
+  title: string
+  value: number
+  type: EType
+}
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .map(i => (i.type === 'income' ? i.value : 0))
+      .reduce((red, inc) => red + inc, 0)
+
+    const outcome = this.transactions
+      .map(i => (i.type === 'outcome' ? i.value : 0))
+      .reduce((red, inc) => red + inc, 0)
+
+    const total = income - outcome
+
+    return { income, outcome, total }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const balance = this.getBalance()
+
+    if (type === 'outcome') {
+      if (balance.total < value) {
+        throw Error('Transaction invalid, no have money suficiently')
+      }
+    }
+
+    const transaction = new Transaction({ title, type, value })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,40 @@
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+enum EType {
+  i = 'income',
+  o = 'outcome',
+}
+
+interface Balance {
+  title: string
+  value: number
+  type: EType
+}
+// EType.i
 
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private transactionsRepository: TransactionsRepository
 
   constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+    this.transactionsRepository = transactionsRepository
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Balance): Transaction {
+    const { i } = EType
+    const { o } = EType
+
+    if (type === i || type === o) {
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      })
+
+      return transaction
+    }
+    throw Error('The type is not income or outcome.')
   }
 }
 
-export default CreateTransactionService;
+export default CreateTransactionService
